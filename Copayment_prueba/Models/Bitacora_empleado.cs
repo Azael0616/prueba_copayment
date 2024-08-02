14638938d66bec16e8b98e7d8a182c375f2bf197
Azael0616 <sect1965@gmail.com>
2024-08-01 21:04:28 -0600

    Se agregaron los modelos

---
+++ Copayment_prueba/Models/Bitacora_empleado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Copayment_prueba.Models
+{
+    public class Bitacora_empleado
+    {
+        public int Id_registro {  get; set; }
+        public int Id_empleado { get; set; }
+        public DateTime Entrada { get; set; }
+        public DateTime Salida { get; set; }
+
+        public Bitacora_empleado() { }
+        public Bitacora_empleado(int id_reg,int id_emp,DateTime entrada, DateTime salida) { 
+            this.Id_registro = id_reg;
+            this.Id_empleado = id_emp;
+            this.Entrada = entrada;
+            this.Salida = salida;
+        }
+    }
+}
---
+++ Copayment_prueba/Models/Empleado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Copayment_prueba.Models
+{
+    public class Empleado
+    {
+        public int Id_empleado {  get; set; }
+        public string Nombre { get; set; }
+        public string App { get; set; }
+        public string Apm {  get; set; }
+
+        public DateTime Fecha_nacimiento { get; set; }
+        public string Sexo { get; set; }
+        public int Departamento { get; set; }
+        public string Telefono_contacto { get; set; }
+        public string Correo_contacto { get; set; }
+        public string Activo { get; set; }
+        public Empleado() { }
+        public Empleado (int id_emp, string nom, string app, string apm, DateTime nacimiento,string sex, int dep, string telefono, string correo, string act)
+        {
+            this.Id_empleado = id_emp;
+            this.Nombre = nom;
+            this.App = app;
+            this.Apm= apm;
+            this.Fecha_nacimiento= nacimiento;
+            this.Sexo = sex;
+            this.Departamento = dep;
+            this.Telefono_contacto = telefono;
+            this.Correo_contacto = correo;
+            this.Activo = act;
+
+        }
+    }
+}
