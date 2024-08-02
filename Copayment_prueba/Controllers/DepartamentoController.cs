503882cc4088c6496b4198adcc0612abedfb9d11
Azael0616 <sect1965@gmail.com>
2024-08-02 01:29:13 -0600

    Controlador y DAL de Departamento

--- Copayment_prueba/Controllers/DepartamentoController.cs
+++ Copayment_prueba/Controllers/DepartamentoController.cs
@@ -1,19 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
+using Copayment_prueba.Models;
+using Copayment_prueba.DAL;
+using System;
+using Copayment_prueba.Dtos;
 
 namespace Copayment_prueba.Controllers
 {
     public class DepartamentoController : ApiController
     {
-        [HttpGet]
-        [Route("api/departamento/default")]
-        public IHttpActionResult GetDefault()
+        private DepartamentoDAL _departamentoDAL = new DepartamentoDAL();
+        #region Obtener departamento
+        [HttpPost]
+        [Route("api/departamento/obtenerDepartamento/{id}")]
+        public Departamento obtenerDepartamento(int id)
         {
-            return Ok("Default endpoint para el controlador Departamento");
+            Departamento departamento = _departamentoDAL.obtenerDepartamento(id);
+            return departamento;
         }
+        [HttpPost]
+        [Route("api/departamento/obtenerDepartamentos")]
+        public List<Departamento> obtenerDepartamentos()
+        {
+            List<Departamento> departamentos = _departamentoDAL.obtenerDepartamentos();
+            return departamentos;
+        }
+        #endregion
+        #region Actualizar departamento
+        [HttpPost]
+        [Route("api/departamento/actualizarDepartamento")]
+        public string actualizarDepartamento(Departamento parametro)
+        {
+            return _departamentoDAL.actualizarDepartamento(parametro);
+        }
+        [HttpPost]
+        [Route("api/departamento/actualizarPagoHora")]
+        public string actualizarPagoHora(Departamento parametro)
+        {
+            return _departamentoDAL.actualizarPagoHora(parametro);
+        }
+        #endregion
+        #region Eliminar departamento
+        [HttpPost]
+        [Route("api/departamento/eliminarDepartamento/{id}")]
+        public string eliminarDepartamento(int id)
+        {
+            return _departamentoDAL.eliminarDepartamento(id);
+        }
+        #endregion
     }
 }
---
+++ Copayment_prueba/DAL/DepartamentoDAL.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Data;
+using Copayment_prueba.Models;
+using Copayment_prueba.Dtos;
+
+namespace Copayment_prueba.DAL
+{
+    public class DepartamentoDAL
+    {
+        #region Obtener Departamento
+        public Departamento obtenerDepartamento(int id)
+        {
+            DataTable dtDepartamentos = new DataTable();
+            Departamento departamento = new Departamento();
+            using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+            {
+                using (MySqlCommand command = new MySqlCommand("obtenerDepartamento", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@id", id);
+
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        connection.Open();
+                        adapter.Fill(dtDepartamentos);
+                        connection.Close();
+                    }
+                }
+            }
+            if (dtDepartamentos?.Rows?.Count > 0)
+            {
+                departamento.Id_departamento = Convert.ToInt32(dtDepartamentos.Rows[0]["Id_departamento"]);
+                departamento.Nombre = dtDepartamentos.Rows[0]["Nombre"].ToString();
+                departamento.Pago_hora = Convert.ToDouble(dtDepartamentos.Rows[0]["Pago_hora"].ToString());
+                return departamento;
+            }
+            else
+                return departamento;
+        }
+        public List<Departamento> obtenerDepartamentos()
+        {
+            DataTable dtDepartamentos = new DataTable();
+            List<Departamento> departamentos = new List<Departamento>();
+            using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+            {
+                using (MySqlCommand command = new MySqlCommand("obtenerDepartamentos", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        connection.Open();
+                        adapter.Fill(dtDepartamentos);
+                        connection.Close();
+                    }
+                }
+            }
+            if (dtDepartamentos?.Rows?.Count > 0)
+            {
+                foreach (DataRow row in dtDepartamentos.Rows)
+                {
+                    Departamento departamento = new Departamento();
+
+                    departamento.Id_departamento = Convert.ToInt32(row["Id_departamento"]);
+                    departamento.Nombre = row["Nombre"].ToString();
+                    departamento.Pago_hora = Convert.ToDouble(row["Pago_hora"].ToString());
+
+                    departamentos.Add(departamento);
+                }                
+                return departamentos;
+            }
+            else
+                return departamentos;
+        }
+        #endregion
+        #region Actualizar Departamento
+        public string actualizarDepartamento(Departamento parametro)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+                {
+                    using (MySqlCommand command = new MySqlCommand("actualizarDepartamento", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@id", parametro.Id_departamento);
+                        command.Parameters.AddWithValue("@nom", parametro.Nombre);
+                        command.Parameters.AddWithValue("@pago", parametro.Pago_hora);                        
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                return "Departamento actualizado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Ha ocurrido un error: " + ex.Message.ToString();
+            }
+        }
+        public string actualizarPagoHora(Departamento parametro)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+                {
+                    using (MySqlCommand command = new MySqlCommand("actualizarPagoHora", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@id", parametro.Id_departamento);                        
+                        command.Parameters.AddWithValue("@pago", parametro.Pago_hora);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                return "Pago por hora actualizado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Ha ocurrido un error: " + ex.Message.ToString();
+            }
+        }
+        #endregion
+        #region Eliminar departamento
+        public string eliminarDepartamento(int id)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+                {
+                    using (MySqlCommand command = new MySqlCommand("eliminarDepartamento", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@id", id);                        
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                return "Departamento eliminado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Ha ocurrido un error: " + ex.Message.ToString();
+            }
+        }
+        #endregion
+    }
+}
