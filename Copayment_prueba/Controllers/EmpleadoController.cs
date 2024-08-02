fff97cc4f373b5dd36f7975d188597d69d91950b
Azael0616 <sect1965@gmail.com>
2024-08-01 22:36:25 -0600

    Controlador y DAL de Empleados

--- Copayment_prueba/Controllers/EmpleadoController.cs
+++ Copayment_prueba/Controllers/EmpleadoController.cs
@@ -4,16 +4,55 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-
+using Copayment_prueba.Models;
+using Copayment_prueba.DAL;
 namespace Copayment_prueba.Controllers
 {
     public class EmpleadoController : ApiController
     {
-        [HttpGet]
-        [Route("api/empleado/default")]
-        public IHttpActionResult GetDefault()
+        private EmpleadosDAL _empleadosDAL = new EmpleadosDAL();
+        #region Obtener empleado
+        [HttpPost]
+        [Route("api/empleado/obtenerEmpleado/{id}")]
+        public Empleado obtenerEmpleado(int id)
         {
-            return Ok("Default endpoint para el controlador Empleado");
+                Empleado empleado = _empleadosDAL.obtenerEmpleado(id);
+                return empleado;            
         }
+        [HttpPost]
+        [Route("api/empleado/obtenerEmpleados")]
+        public List<Empleado> obtenerEmpleados()
+        {
+            List<Empleado> empleados = _empleadosDAL.obtenerEmpleados();
+            return empleados;
+        }
+        #endregion
+
+        #region Crear empleado
+        [HttpPost]
+        [Route("api/empleado/crearEmpleado")]
+        public string crearEmpleado(Empleado parametro)
+        {
+            return _empleadosDAL.crearEmpleado(parametro);
+        }
+        #endregion
+
+        #region Actualizar empleado
+        [HttpPost]
+        [Route("api/empleado/actualizarEmpleado")]
+        public string actualizarEmpleado(Empleado parametro)
+        {
+            return _empleadosDAL.actualizarEmpleado(parametro);
+        }
+        #endregion
+        #region Eliminar empleado
+        [HttpPost]
+        [Route("api/empleado/eliminarEmpleado/{id}")]
+        public string eliminarEmpleado(int id)
+        {
+            return _empleadosDAL.eliminarEmpleado(id);
+        }
+        #endregion
+
     }
 }
---
+++ Copayment_prueba/DAL/EmpleadosDAL.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Data.SqlClient;
+using Copayment_prueba.Models;
+using Copayment_prueba.Utils;
+using System.Net;
+
+namespace Copayment_prueba.DAL
+{
+    public class EmpleadosDAL
+    {
+        public Empleado obtenerEmpleado(int id_empleado)
+        {
+            DataTable dtEmpleado = new DataTable();                
+            Empleado emp = new Empleado();
+            using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+            {
+                using (MySqlCommand command = new MySqlCommand("obtenerEmpleado", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@id", id_empleado);
+
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {                        
+                        connection.Open();
+                        adapter.Fill(dtEmpleado);
+                        connection.Close();                        
+                    }
+                }
+            }
+            if (dtEmpleado?.Rows?.Count > 0)
+            {
+                emp.Id_empleado = Convert.ToInt32(dtEmpleado.Rows[0]["Id_empleado"]);
+                emp.Nombre = dtEmpleado.Rows[0]["Nombre"].ToString();
+                emp.App = dtEmpleado.Rows[0]["App"].ToString();
+                emp.Apm = dtEmpleado.Rows[0]["Apm"].ToString();
+                emp.Fecha_nacimiento = Convert.ToDateTime(dtEmpleado.Rows[0]["Fecha_nacimiento"]);
+                emp.Sexo = dtEmpleado.Rows[0]["Sexo"].ToString();
+                emp.Departamento = Convert.ToInt32(dtEmpleado.Rows[0]["Departamento"]);
+                emp.Telefono_contacto = dtEmpleado.Rows[0]["Telefono_contacto"].ToString();
+                emp.Correo_contacto = dtEmpleado.Rows[0]["Correo_contacto"].ToString();
+                emp.Activo = dtEmpleado.Rows[0]["Activo"].ToString();
+                return emp;
+            }
+            else
+            {                
+                return emp;
+            }
+        }
+        public List<Empleado> obtenerEmpleados()
+        {
+            DataTable dtEmpleado = new DataTable();
+            List<Empleado> empleados = new List<Empleado>();
+            using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+            {
+                using (MySqlCommand command = new MySqlCommand("obtenerEmpleados", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+                    {
+                        connection.Open();
+                        adapter.Fill(dtEmpleado);
+                        connection.Close();
+                    }
+                }
+            }
+            if (dtEmpleado?.Rows?.Count > 0)
+            {                
+                foreach (DataRow row in dtEmpleado.Rows)
+                {
+                    Empleado emp = new Empleado();                    
+                        
+                    emp.Id_empleado = Convert.ToInt32(row["Id_empleado"]);
+                    emp.Nombre = row["Nombre"].ToString();
+                    emp.App = row["App"].ToString();
+                    emp.Apm = row["Apm"].ToString();
+                    emp.Fecha_nacimiento = Convert.ToDateTime(dtEmpleado.Rows[0]["Fecha_nacimiento"]);
+                    emp.Sexo = row["Sexo"].ToString();
+                    emp.Departamento = Convert.ToInt32(dtEmpleado.Rows[0]["Departamento"]);
+                    emp.Telefono_contacto = row["Telefono_contacto"].ToString();
+                    emp.Correo_contacto = row["Correo_contacto"].ToString();
+                    emp.Activo = row["Activo"].ToString();                    
+
+                    empleados.Add(emp);
+                }                                
+                return empleados;
+            }
+            else
+            {                
+                return empleados;
+            }
+        }
+        public string crearEmpleado(Empleado parametro)
+        {
+            try { 
+            using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+            {
+                using (MySqlCommand command = new MySqlCommand("crearEmpleado", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    
+                    command.Parameters.AddWithValue("@nom", parametro.Nombre);
+                    command.Parameters.AddWithValue("@app", parametro.App);
+                    command.Parameters.AddWithValue("@apm", parametro.Apm);
+                    command.Parameters.AddWithValue("@nacimiento", parametro.Fecha_nacimiento);
+                    command.Parameters.AddWithValue("@sex", parametro.Sexo);
+                    command.Parameters.AddWithValue("@dep", parametro.Departamento);
+                    command.Parameters.AddWithValue("@telefono", parametro.Telefono_contacto);
+                    command.Parameters.AddWithValue("@correo", parametro.Correo_contacto);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+                return "Empleado creado correctamente";
+            }
+            catch (Exception ex) {
+                return "Ha ocurrido un error: "+ex.Message.ToString();
+            }
+        }
+        public string actualizarEmpleado(Empleado parametro)
+        {
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+                {
+                    using (MySqlCommand command = new MySqlCommand("actualizarEmpleado", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("@id", parametro.Id_empleado);
+                        command.Parameters.AddWithValue("@nom", parametro.Nombre);
+                        command.Parameters.AddWithValue("@app", parametro.App);
+                        command.Parameters.AddWithValue("@apm", parametro.Apm);
+                        command.Parameters.AddWithValue("@nacimiento", parametro.Fecha_nacimiento);
+                        command.Parameters.AddWithValue("@sex", parametro.Sexo);
+                        command.Parameters.AddWithValue("@dep", parametro.Departamento);
+                        command.Parameters.AddWithValue("@telefono", parametro.Telefono_contacto);
+                        command.Parameters.AddWithValue("@correo", parametro.Correo_contacto);
+                        command.Parameters.AddWithValue("@act", parametro.Activo);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                return "Empleado actualizado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Ha ocurrido un error: " + ex.Message.ToString();
+            }
+        }
+
+        public string eliminarEmpleado(int id_emp)
+        {
+            Empleado emp = obtenerEmpleado(id_emp);
+            if (emp.Id_empleado>0)
+            {
+                try
+                {
+                    using (MySqlConnection connection = new MySqlConnection(Utils.Utils.cadenaConexion()))
+                    {
+                        using (MySqlCommand command = new MySqlCommand("eliminarEmpleado", connection))
+                        {
+                            command.CommandType = CommandType.StoredProcedure;
+                            command.Parameters.AddWithValue("@id", id_emp);
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                            connection.Close();
+                        }
+                    }
+                    return "Empleado eliminado correctamente";
+                }
+                catch (Exception ex)
+                {
+                    return "Ha ocurrido un error: " + ex.Message.ToString();
+                }
+            }
+            else
+                return "El empleado no existe";
+            
+        }
+    }
+}
+
